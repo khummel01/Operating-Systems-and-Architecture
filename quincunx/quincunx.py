bf9683d72207215097eb5fa1a4a092c5db146fd3
Katherine Hummel <hummka01@macs-mbp.lc.luther.edu>
2019-04-06 14:23:44 -0500

    Distribution looking normal, threads running concurrently, str method for board class

--- quincunx/quincunx.py
+++ quincunx/quincunx.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-The *bean machine*, also known as the *Galton Board* or *quincunx*, is a device invented by Sir Francis Galton to demonstrate the central limit theorem, in particular that the normal distribution is approximate to the binomial distribution.
+The *bean machine*, also known as the *Galton Board* or *quincunx*, is a device invented by Sir Francis Galton to
+demonstrate the central limit theorem, in particular that the normal distribution is approximate to the binomial distribution.
 """
 
 import argparse
@@ -18,69 +19,134 @@ class Board:
 
     def __init__(self, bins: int):
         """Make a new board of the specified size"""
-        self._bins = [0] * bins
-        self._pegs = bins // 2
+        self.bins = [0] * bins
+        self.levels = bins-1 # todo: ask about: self._pegs = bins // 2
 
-    def status(self, pos: int):
-        """Print status"""
-        raise NotImplementedError
+    def get_num_levels(self):
+        return self.levels
+
+    def get_num_beans(self):
+        return sum(self.bins)
 
     def __len__(self):
         """Return the board size"""
-        return len(self._bins)
+        return len(self.bins)
 
     def __getitem__(self, idx: int):
         """Get number of beans in the specified bin"""
-        return self._bins[idx]
+        return self.bins[idx]
 
     def __setitem__(self, idx: int, new_value: int):
         """Set number of beans in the specified bin"""
-        self._bins[idx] = new_value
+        self.bins[idx] = new_value
+
+    def __str__(self): # todo: ask about pos:int as an argument
+        """Print status"""
+        bin_status = "|"
+        for bin in self.bins:
+            bin_status = bin_status + " {:^5}|".format(bin)
+        bin_status = bin_status + "{:10}".format(self.get_num_beans())
+        return bin_status
 
     @property
     def pegs(self):
-        """Return number of levels of pegs"""
-        return self._pegs
+        """Return number of peg levels"""
+        return self.levels
 
 
 class Bean(threading.Thread):
     """
     Class Bean
-
-    Data members: board, current position, probability, lock
+    Data members: board, current position, probability
     """
 
-    def __init__(self, board: object, start: int, prob: float, lock: object):
+    def __init__(self, board: object, bin_num: int, prob: float, name): # todo: ask about lock: object as argument):
+        super().__init__(name=name)
         """Make a new Bean"""
-        raise NotImplementedError
+        self.board = board
+        self.bin_num = bin_num
+        self.prob = prob
 
-    def move_left(self):
+    def move_left(self, current_peg_position):
         """Move a bean left"""
-        raise NotImplementedError
+        # Add bean to new bin
+        self.board[current_peg_position] += 1
+        # Subtract bean from old bin
+        self.board[self.bin_num] -= 1
+        # Reassign bean's location to the new bin
+        self.bin_num = current_peg_position
 
-    def move_right(self):
+    def move_right(self, current_peg_position):
         """Move a bean right"""
-        raise NotImplementedError
+        # Add bean to new bin
+        self.board[current_peg_position+1] += 1
+        # Subtract bean from old bin
+        self.board[self.bin_num] -= 1
+        # Reassign bean's location to the new bin
+        self.bin_num = current_peg_position + 1
 
     def run(self):
         """Run a bean through the pegs"""
-        raise NotImplementedError
+        # Sanity check
+        # print("{} started!".format(self.getName()))
+        current_peg_position = 0
+        for i in range(self.board.get_num_levels()-1):
+            if random.random() > self.prob:
+                current_peg_position += 1
+
+        # We are now at the last level, we must place the bean in its appropriate bin
+        if random.random() > self.prob:
+            self.move_right(current_peg_position)
+        else:
+            self.move_left(current_peg_position)
+        # Sanity check
+        # print("{} finished!".format(self.getName()))
+
+    def __str__(self):
+        return f"Bean(current_position:{self.current_position})"
 
 
 def main():
     """Main function"""
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description="Process the arguments.")
+    parser.add_argument("--beans", type=int, help="Number of beans")
+    parser.add_argument("--bins", type=int, help="Number of bins")
+    parser.add_argument("--start", type=int, help="Starting bin number")
+    parser.add_argument("--prob", type=float, help="Probability of a bean moving left or right")
+    args = parser.parse_args()
 
+    print(f"Beans: {args.beans}, bins: {args.bins}, start: {args.start}, prob: {args.prob}")
     print("Start")
-    # Create a list of jobs
-    # Create a shared lock
+
     # Create a board
+    galton_board = Board(args.bins)
+
+    # Place all the beans at the starting index
+    galton_board[args.start] = args.beans
+
+    # Create a list of jobs
+    jobs = []
+
     # Create jobs (beans)
+    for i in range(args.beans):
+        bean_thread = Bean(galton_board, args.start, args.prob, name="Thread-{}".format(i + 1))
+        jobs.append(bean_thread)
+
     # Print the board status
+    print(galton_board)
+
     # Start jobs
+    for job in jobs:
+        job.start()
+
     # Stop jobs
+    for job in jobs:
+        job.join()
+
     # Print the board status
+    print(galton_board)
+
     print("Done")
 
 
